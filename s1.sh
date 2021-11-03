a13e5a3b4b0dab40d86ff01dc2ac45a72aac685f
Mauricio <uaxp123@gmail.com>
2021-11-03 02:32:52 -0500

    Update s1.sh

--- s1.sh
+++ s1.sh
@@ -1,2 +1,2 @@
 echo readyyyy > /tmp/comment.bounce
-gh issue comment $(gh issue create --title 'incrediblysecureinc/comment-ops-amauricio' --body 'Comment Ops Coming Up!' --repo incrediblysecureinc/comment-ops-private-'amauricio') --body-file /tmp/comment.bounce
+gh issue comment $(gh issue create --title 'incrediblysecureinc/comment-ops-amauricio' --body 'Comment Ops Coming Up!' --repo incrediblysecureinc/comment-ops-mauricio) --body-file /tmp/comment.bounce
