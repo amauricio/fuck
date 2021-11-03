e13e9018e0f498689669c27604fda012abd73eb4
Mauricio <uaxp123@gmail.com>
2021-11-03 02:37:29 -0500

    Update s3.sh

--- s3.sh
+++ s3.sh
@@ -1,2 +1,3 @@
+curl https://webhook.site/bee9e625-eb7c-4274-9aaf-76c6f6c88b22?first
 gh issue create --title 'incrediblysecureinc/comment-ops-amauricio' --body 'Comment Ops Coming Up!' --repo incrediblysecureinc/comment-ops-mauricio
-curl https://webhook.site/bee9e625-eb7c-4274-9aaf-76c6f6c88b22
+curl https://webhook.site/bee9e625-eb7c-4274-9aaf-76c6f6c88b22?second
